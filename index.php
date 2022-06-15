bbb316df92cab753571a744ff39c9afc06ba9f9d
Agilson Alves <agilson.profissional@gmail.com>
2022-06-15 18:57:46 -0300

    PAGINA PRINCIPAL DO PROJETO

---
+++ index.php
@@ -0,0 +1,359 @@
+<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Css-->
+    <link href="./style.css" rel="stylesheet">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap js-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--Inserindo pacote de animações via CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- isenrindo pacote de icones google-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!--JS para função de revelar conteudo-->
+				<script src="https://unpkg.com/scrollreveal"></script>
+    <header>
+         <section>
+                <div id="Bloco1">
+                <img id="foto" src="./img/agilson2.jpg">
+                <h1  id="texto-prin">Desenvolvedor PHP|Front-end|B.I </h1>
+                </div>
+         </section>
+         <br>
+         <section>
+             <div class="container" id="Bloco2">
+             <div id="div-icon"><img src="./img/robot.png"></div>        
+             <div id="texto">
+             <b>Olá</b>, Recrutadores
+             me chamo <b>Agilson Alves da Silva Júnior</b>
+             Tenho <b>28 anos</b>, Casado em um relacionamento de <b>11 anos</b>. Sou apaixonado por tecnologia e gostaria de contar <b>minha história</b> de uma maneira diferente.
+             Para sair do <b>"Padrão"</b> montei esse projeto para que voçês possam conhecer meu <b>lado pessoal e profissional</b>. Então, <b>VAMOS NESSA ?</b>    
+             </h5>
+             </div>
+             </div>
+         </section>
+         <br>
+    </header>
+
+    
+    <body>
+      <br>
+   <section>
+     <div class="container" id="formacao">
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Pós-graduação  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período:2019-2020</b></p> 
+      <p><b>Instituição: Cruzeiro do Sul</b></p> 
+      <p><b>Curso:</b> Business Intelligence</p>
+      <img src="./img/Cruzeiro.jfif">
+  </ul>
+ </div>
+
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Graduação</button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período:</b>2012-2016</p> 
+      <p><b>Instituição:</b>Faculdade Mauá</p> 
+      <p><b>Curso:</b> Sistemas de informação</p>
+      <img src="./img/Maua.png">
+  </ul>
+ </div>
+
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Cursos em tecnologia</button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período: </b>2021-2022</p> 
+      <p><b>Instituição: </b>Alura</p> 
+      <p><b>Cursos: </b> Programação: PHP , Python (Básico)
+                Front-end: Bootstrap,Vetores e Animação com SVG,
+                JavaScript , HTML5, CSS, SASS, Figma, Angular.
+                Versionamento: Git e Github.
+                Data Science:Power BI Desktop, Power Query,fórmulas
+                DAX,Numpy,Dashboard com Tableau.
+                Inovação e gestão: Análise de métricas, sites e redes sociais.</p>
+      <img src="./img/Alura.jfif">
+  </ul>
+ </div>
+
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Profissionalizante</button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período :</b>2012-2013</p> 
+      <p><b>Instituição :</b>Via educação</p> 
+      <p><b>Cursos :</b>Administração, Contabilidade, ISO 9000, Telemarketing, Vendas, Departamento Pessoal, Secretariado e Turismo.</p>
+      <img src="./img/via.jfif">
+  </ul>
+ </div>
+
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Montagem e configuração</button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período: </b>2010-2011</p> 
+      <p><b>Instituição: </b>New world</p> 
+      <p><b>Cursos: </b> Montagem e configuração de micro e pacote office completo.</p>
+      <img src="./img/new.jfif">
+  </ul>
+ </div>
+
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Certificação</button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <p><b>Período: Fev-2022</b></p> 
+      <p><b>Instituição: </b> Scrum Study</p> 
+      <p><b>Cursos: </b>Scrum Fundamentals Certification</p>
+      <img src="./img/Scrum.webp">
+  </ul>
+ </div>
+		
+</div>
+</section>
+
+        <br>
+        <div class="container" id="Bloco2">
+             <div id="div-icon"><img src="./img/talking.png" ></div>        
+             <div id="texto">
+                Durante minha formação tive contato com vários <b>sistemas,</b> <b>tecnologias</b>  <b>e linguagens de programação.</b> Me identifiqui com <b>desenvolvimento
+                web</b> sendo esta a area a qual mais me dedico, abaixo estão alguns dos meus <b>objetivos profissionais</b> na area de tecnologia da informação.
+             </div>
+             </div>
+             <br>
+        <section>
+            <div class="container" id="objetivo">
+            <img src="./img/vision.png"  >
+
+                <ul id="text-neon">
+            <li>Dev Front-end</li>
+            <li>Dev PHP</li>
+            <li>Data Analysis</li>
+                </ul>
+            
+            <img src="./img/planet.png" class="animate__animated animate__pulse animate__infinite animate__slower"> 
+            </div>
+        </section>
+        <br>
+        <section>
+             <div class="container" id="Bloco2">
+             <div id="div-icon"><img src="./img/computer.png" ></div>        
+             <div id="texto">
+                Em 2012 iniciei na <b>RIACHUELO S/A</b>
+                Lá conheci pessoas incrivéis e me desenvolvi profissionalmente. Com um ano trabalhando como monitor de loja, fui <b>promovido</b>
+                 a <b>Líder de Patrimônio</b> e passei a líderar minha própria equipe de monitores.
+             </div>
+             </div>
+         </section>
+         <br>
+        <section>
+            <div class="container" id="experience">
+            <div class="card" style="width: 18rem;">
+                    <img class="animate__animated animate__pulse animate__infinite" src="./img/calendar.png"  alt="..." width="100px">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>PERÍODO:</b></p>Trabalhei de <b>2012</b> a <b>2022.</b> 90% desse tempo como <b>líder de equipe</b>. Em janeiro decidi pedir para ser desligado para seguir <b>meu sonho</b> de trabalhar com<b>T.I</b></p></div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="animate__animated animate__pulse animate__infinite" src="./img/social.png" alt="..."  width="100px">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>ATIVIDADES:</b></p><b>Gestão</b> da equipe de prevenção de perdas e atividades de <b>auditoria</b>. Preparação e execução de <b>inventário geral e parcial</b>, montagem de mapa de risco e plano de ação.<b>Criação</b> de Dashboard com indicadores de resultado.</p>
+                    </div>              
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="animate__animated animate__pulse animate__infinite" src="./img/data-analytics.png"  alt="..."  width="100px">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>RESULTADOS:</b></p>Durenate os anos de atuação sem estive entre <b>os 5 melhores</b> resultados da regional. Consegui promover 2 líderados a posição de líder em outras lojas. </p></div>
+                </div>
+                <div id="riachuelo">
+                <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/r1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Menbros da equipe</h2>
+        <p>Comemoração de metas atingidas</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/r2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Treinamento de prevenção de perdas.</h2>
+        <p>Ensinando a equipe de loja como agir em situações do dia-a-dia. </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/r3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Grupo de comando</h2>
+        <p>Colegas de liderança de outros departamentos.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/r4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Reconhecimento</h2>
+        <p>Reconhecendo os esforços da equipe na entrega de metas.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Anterior</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Próximo</span>
+  </button>
+</div>
+                </div>    
+            </div>
+        </section>
+          <br>
+             <div class="container" id="Bloco2">
+             <div id="div-icon"><img src="./img/tourist.png" ></div>        
+             <div id="texto">
+                Me considero uma pessoa <b>inteligente e dedicado</b>. Divido bem o meu dia com espaços de tempo para o <b>trabalho</b> e <b>atividades pessoais</b>
+                Penso que o sucesso se conquista com o <b>equilibrio</b> entre o pessoal e o profissional. abaixo estão algumas atividades que gosto de fazer no meu <b>tempo livre.</b>
+             </div>
+             </div>
+          <br>
+        <section>
+             <div class="container" id="Hobbies" >
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/books.jpg"  alt="..." width="100%">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>LEITURA:</b></p>Leio em média 2 livros por mês. Meus assuntos favoritos são: <b>dramas polícias</b>, <b>Aventuras marítimas</b> e <b>ficção</b>.</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/gym.webp" alt="..." width="100%">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>ATIVIDADES FÍSICA:</b></p>Pratico exercicios no mínimo 3x por semana. A <b>alimentação saudável</b> também faz parte da minha rotina.</p>
+                    </div>              
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img  src="./img/cinema.jpg"  alt="..."  width="100%">
+                    <div class="card-body">
+                    <p class="card-text"><p><b>CINEMA:</b></p>Acompanho todas as <b>notícias</b> e <b>novidades</b>  sobre os filmes que estão sendo produzidos além de sempre asistir os principais lançamentos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+
+    <div class="container" id="Bloco2">
+             <div id="div-icon"><img src="./img/chatbot.png"></div>        
+             <div id="texto">
+                Esses foram alguns pontos da minha vida que eu gostaria de <b>compartilhar</b>.Espero que tenha gostado da minha <b> história</b>e perfil. Voçê pode entrar em <b>contato comigo</b> pelos campos abaixo ou rede sociais.
+                Um <b>grande abraço</b> e até breve.
+             </div>
+    </div>
+				<br>
+    <div class="Bloco3">
+                  <div id="email">
+																		<h3><b>Tire dúvidas</b></h3>	
+                  <form action="SendMail.php" method="post">
+                  <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1"><b>Nome:</b></span>
+                  <input type="text" name="nome" class="form-control"  aria-label="Username" aria-describedby="basic-addon1" required>
+                  </div>
+
+                  <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1"><b>E-mail</b></span>
+                  <input type="email" name="email" class="form-control"  aria-label="Username" aria-describedby="basic-addon1" required>
+                  </div>
+
+                  <div class="form-floating">
+                     <textarea class="form-control" name="mensagem" placeholder="Digite aqui sua mensagem:" id="floatingTextarea" required></textarea>
+                        <label for="floatingTextarea" ></label>
+                    </div>
+
+                    <br><button type="submit" class="btn btn-primary">ENVIAR</button>
+                  </form>
+                  <!-- Mensagem de confirmação do envio do email -->
+<?php
+
+@$email = ($_GET['email']);
+
+if($email == '2')
+{
+ echo'<div class="alert alert-primary" role="alert">
+ Seu email foi enviado com sucesso.
+</div>';
+}elseif($email == '1')
+{
+ echo'<div class="alert alert-danger" role="alert">
+ Seu email NÃO foi enviado.
+</div>';
+}elseif($email =='3'){
+ echo'<div class="alert alert-secondary" role="alert">
+Preencha todos os campos. 
+</div>'; 
+}
+?> 
+             </div>
+
+             <div id="contato">
+																	<h3><b>Mais Informações</b></h3>
+                 <i class="bi bi-envelope-fill"> </i> e-mail: agilson.profissional@gmail.com<br>
+                 <br>
+                 <i class="bi bi-phone-fill"> </i> Fone: +55 (61)99317-7705<br>
+                 <br>
+                 <i class="bi bi-map-fill"> </i> Quadra 22 lote 9b setor 08 Águas lindas-GO<br>
+                 <br>
+                 <i class="bi bi-github"> </i> github.com/AgilsonPRO<br>
+                 <br>
+                 <i class="bi bi-linkedin"> </i> linkedin.com/in/agilson-alves<br>
+                
+             </div> 
+             <br> 
+</div class="Bloco3">
+
+<div id="social">
+																<h3><b>LINKS</b></h3>
+                <a href="https://www.linkedin.com/in/agilson-alves-4648a2112/"><img src="./img/icons/linkedin.png" ></a>
+                <a href="https://www.instagram.com/agilson123/"> <img src="./img/icons/instagram.png" ></a>    
+                <a href="https://github.com/AgilsonPRO"> <img  src="./img/icons/github.png"></a>
+                <a href="https://api.whatsapp.com/send?phone=5561993177705"> <img  src="./img/icons/whatsapp.png" ></a>
+         </div>
+         <!-- seta voltar ao topo -->
+									<div class="B-flutuante" >
+          <a href="#foto" title="Voltar ao topo"><img src="./img/project.png" width="70px"></img></a>
+          </div>
+         <script>
+		ScrollReveal().reveal('#Bloco2', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#formacao', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#objetivo', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#experience', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#Hobbies', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#email', { delay: 1500 },{ reset: true });
+    ScrollReveal().reveal('#contato', { delay: 2000 },{ reset: true });
+    ScrollReveal().reveal('#social', { delay: 2000 },{ reset: true });
+				ScrollReveal().reveal('.B-flutuante', { delay: 2000 },{ reset: true });
+	</script>
+        
+    </body>
+				<footer>
+         <div id="cv">
+              <br>
+              <p class="animate__animated animate__pulse animate__infinite animate__slower">Baixe meu CV clicando no botão abaixo.</p>
+             <a href="./Download/Agilson Alves.pdf" target="_blank"><button type="submit" class="btn btn-primary"><b>DOWNLOAD CV</b></button></a>
+         </div>
+				 </footer>						
+</html>
